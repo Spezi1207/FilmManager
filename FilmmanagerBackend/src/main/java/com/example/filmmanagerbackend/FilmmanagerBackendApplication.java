de0cf285d93afd875d7495155c6eca8a1330b4f1
Poelzleitner <poelzleitner.lukas@gmail.com>
2026-03-23 11:39:24 +0100

    fix

---
+++ FilmmanagerBackend/src/main/java/com/example/filmmanagerbackend/FilmmanagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.filmmanagerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmmanagerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmmanagerBackendApplication.class, args);
+    }
+
+}
---
+++ FilmmanagerBackend/src/test/java/com/example/filmmanagerbackend/FilmmanagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filmmanagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmmanagerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
