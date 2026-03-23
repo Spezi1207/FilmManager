5ad4c9161ed6255cee05b7c794df9e16d511861e
Poelzleitner <poelzleitner.lukas@gmail.com>
2026-03-23 11:59:51 +0100

    ints und strings

--- FilmmanagerBackend/src/main/java/com/example/filmmanagerbackend/Filme/Film.java
+++ FilmmanagerBackend/src/main/java/com/example/filmmanagerbackend/Filme/Film.java
@@ -1,4 +1,24 @@
 package com.example.filmmanagerbackend.Filme;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import java.time.LocalDate;
 
+@Entity
+@Table
 public class Film {
+@Id
+
+@GeneratedValue(
+        strategy = GenerationType.IDENTITY
+)
+
+    private Integer id;
+    private String title;
+    private String genre;
+    private Integer year;
+    private Integer duration;
+    private String watchable;
+    private String evaluation;
+
+
 }
--- FilmmanagerBackend/src/main/java/com/example/filmmanagerbackend/Serien/Serie.java
+++ FilmmanagerBackend/src/main/java/com/example/filmmanagerbackend/Serien/Serie.java
@@ -1,4 +1,25 @@
 package com.example.filmmanagerbackend.Serien;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Serie {
+    @Id
+
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY
+    )
+
+    private Integer id;
+    private String title;
+    private String genre;
+    private Integer year;
+    private Integer episodes;
+    private Integer seasons;
+    private String watchable;
+    private String evaluation;
+
+
+
 }
